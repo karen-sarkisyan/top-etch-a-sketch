f3cded3dbb95fa49ef81a4a3b427bafe8352f4e9
karen-sarkisyan <karen.sarkisyan01@gmail.com>
2020-08-14 08:51:56 +0300

    Added functions, boilerplate for validation

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # top-etch-a-sketch
+
+A project, suggested by The Odin Project, to train some skills of manipulating DOM with JavaScript.
+
+It's a nice etch-a-sketch board.
--- script.js
+++ script.js
@@ -1,34 +1,63 @@
-const container = document.querySelector('.gridcontainer')
+// Etch-a-sketch web app
 
-let gridSide = 16;
+const container = document.querySelector('.gridcontainer');
+const inputBox = document.querySelector('#inputBox');
+const buttonReset = document.querySelector('#reset');
+
+let gridSide = parseInt(inputBox.value); 
 let canvasSize = 480;
 
-console.log(canvasSize % gridSide)
+// Initiating grid
 
-const buttonReset = document.querySelector('#reset');
-let inputValue = document.querySelector('#inputBox')
-buttonReset.addEventListener('click', () => console.log(inputValue.value));
+buildGrid(container, canvasSize, gridSide);
 
-/*
-while (canvasSize % gridSide > 0) {
-    console.log('oops!');
+// Validating the input value
 
-    gridSide++;
-};
+inputBox.addEventListener('input', function () {
+    let numberTyped = parseInt(inputBox.value);
+    if (!Number.isInteger(numberTyped) || numberTyped < 16 || numberTyped > 96) {
+        inputBox.style.background = 'red';
+    };
+});
 
-*/
+function validateGridSide(inputSize, canvasSizes) {
 
-for (let i=1; i<=gridSide**2; i++) {
-    const div = document.createElement('div');
-    div.setAttribute('style', 'float:left; background-color: black; opacity: 0; width: 30px; height: 30px;');
-    div.classList.add('gridSquare');
-    container.appendChild(div);
+    while (canvasSizes % inputSize > 0) {
+
+        inputSize++;
+    };
+
+
 }
 
-const listOfSquares = document.querySelectorAll('.gridSquare')
+function buildGrid (canvas, heightOfCanvas, numberRows) {
+    let squareSide = heightOfCanvas/numberRows;
+    for (let i=1; i<=numberRows**2; i++) {
+        const div = document.createElement('div');
+        div.setAttribute('style', `float:left; background-color: black; opacity: 0; width: ${squareSide}px; height: ${squareSide}px;`);
+        div.classList.add('gridSquare');
+        canvas.appendChild(div);
+    }
+}
 
-listOfSquares.forEach((singleSquare) => {
-    singleSquare.addEventListener('mouseenter', function (singleSquare) {
-        singleSquare.target.style.opacity = parseFloat(singleSquare.target.style.opacity) +0.1 ;
+
+
+function makeListeners() {
+    const listOfSquares = document.querySelectorAll('.gridSquare')
+
+    listOfSquares.forEach((singleSquare) => {
+        singleSquare.addEventListener('mouseenter', function (singleSquare) {
+            singleSquare.target.style.opacity = parseFloat(singleSquare.target.style.opacity) +0.1 ;
+        });
     });
-});
+}
+
+makeListeners();
+
+function clearCanvas(canvas) {
+    let listOfChildren = canvas.childNodes;
+    listOfSquares.forEach((child) => {
+        canvas.removeChild(child);
+
+    });
+}
